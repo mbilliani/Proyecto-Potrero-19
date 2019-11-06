f17045a3c7c3af08b8900a6a0739238b32a289df
mbilliani <57192090+mbilliani@users.noreply.github.com>
2019-11-06 14:55:57 -0300

    Add files via upload

---
+++ proyecto_tarde/proyecto_tarde/act_6.php
@@ -0,0 +1,10 @@
+<?php
+
+$resultado=0;
+
+for ($i=2; $i < 21; $i=$i+2) {
+    $resultado= $resultado + $i;
+   echo "$resultado <br>";
+ }
+
+ ?>
---
+++ proyecto_tarde/proyecto_tarde/sumar.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>Sumar</title>
+  </head>
+
+  <body>
+
+ <?php
+ $valor1 = $_POST['T1'];
+ $valor2 = $_POST['T2'];
+ $valor3 = $_POST['T3'];
+
+    $sumar = $valor1 + $valor2 + $valor3;
+
+echo "$valor1 + $valor2 + $valor3 = $sumar";
+
+ ?>
+
+
+  </body>
+</html>
