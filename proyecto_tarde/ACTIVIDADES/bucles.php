43788678cd1a30f09b918e94b7e7d2d472580c74
mbilliani <57192090+mbilliani@users.noreply.github.com>
2019-11-06 16:41:25 -0300

    Add files via upload

---
+++ proyecto_tarde/ACTIVIDADES/bucles.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+$nombres= [ "Anna", "Lourdes", "Clara", "Macarena", "Melisa" ];
+
+for ($i=0; $i < 5 ; $i=$i+1) {
+  echo "$nombres[$i] <br>";
+};
+
+foreach ($nombres as $value) {
+  echo "$value <br>";
+
+};
+
+while ($i < 5 ) {
+  echo "<br> $nombres [$i] <br>";
+$i= $i+1;
+};
+
+//2. Utilizando un While mostrar en pantalla los numeros del 100 al 85
+//3. Con un while mostrar los numeros pares.
+//4. Definir un array con 15 valores numéricos y luego mostrar la suma de todos
+
+
+$i= 100;
+
+while ($i >= 85) {
+  echo "<br> $i <br>";
+  $i= $i-1;
+};
+
+
+$i= 2;
+
+while ($i <= 100) {
+  echo "<br> $i <br>";
+  $i= $i+2;
+};
+
+*/
+
+$numeros= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+$resultado=0;
+
+var_dump($numeros);
+
+
+while ($numeros[$resultado] + ) {
+  echo "<br> $numeros [$resultado] <br>";
+
+$i= $i+1;
+};
+
+
+
+
+
+?>
