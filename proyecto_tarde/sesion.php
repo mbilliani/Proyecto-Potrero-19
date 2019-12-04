9f576dfacf59a073e6b2b2f597ac8d4a9ddb324d
mbilliani <57192090+mbilliani@users.noreply.github.com>
2019-12-04 16:53:17 -0300

    Add files via upload

--- proyecto_tarde/sesion.php
+++ proyecto_tarde/sesion.php
@@ -121,7 +121,7 @@ Released   : 20130920
 	</div>
 </div>
 <div id="copyright">
-  <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+  <p>&copy;   <a href="http://fotogrph.com/"> </a>   <a href="http://templated.co" rel="nofollow"> </a> </p>
 
 	<!-- Redes Sociales -->
 
